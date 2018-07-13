9da77444b3a4526e2aaecd707930a03cd24349e4
astridx <info@astrid-guenther.de>
2018-07-13 23:21:15 +0200

    wip

--- media/com_templates/js/admin-template-compare.js
+++ media/com_templates/js/admin-template-compare.js
@@ -8,159 +8,164 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 (function () {
-  document.addEventListener('DOMContentLoaded', () => {
-    const decodeHtmlspecialChars = function decodeHtmlspecialChars(text) {
-      const map = {
-        '&amp;': '&',
-        '&#038;': '&',
-        '&lt;': '<',
-        '&gt;': '>',
-        '&quot;': '"',
-        '&#039;': "'",
-        '&#8217;': '’',
-        '&#8216;': '‘',
-        '&#8211;': '–',
-        '&#8212;': '—',
-        '&#8230;': '…',
-        '&#8221;': '”',
-      };
+	document.addEventListener('DOMContentLoaded', () => {
+		const decodeHtmlspecialChars = function decodeHtmlspecialChars(text) {
+			const map = {
+				'&amp;': '&',
+				'&#038;': '&',
+				'&lt;': '<',
+				'&gt;': '>',
+				'&quot;': '"',
+				'&#039;': "'",
+				'&#8217;': '’',
+				'&#8216;': '‘',
+				'&#8211;': '–',
+				'&#8212;': '—',
+				'&#8230;': '…',
+				'&#8221;': '”',
+			};
 
-      return text.replace(/\&[\w\d\#]{2,5}\;/g, (m) => {
-        const n = map[m];
-        return n;
-      });
-    };
+			return text.replace(/\&[\w\d\#]{2,5}\;/g, (m) => {
+				const n = map[m];
+				return n;
+			});
+		};
 
-    const compare = function compare(original, changed) {
-      const display = changed.nextElementSibling;
-      let color = '';
-      let pre = null;
-      const diff = JsDiff.diffLines(original.innerHTML, changed.innerHTML);
-      const fragment = document.createDocumentFragment();
+		const compare = function compare(original, changed) {
+			const display = changed.nextElementSibling;
+			let color = '';
+			let pre = null;
+			const diff = JsDiff.diffLines(original.innerHTML, changed.innerHTML);
+			const fragment = document.createDocumentFragment();
 
-      diff.forEach((part) => {
-        if (part.added) {
-          color = '#a6f3a6';
-        } else if (part.removed) {
-          color = '#f8cbcb';
-        } else {
-          color = '';
-        }
-        pre = document.createElement('pre');
-        pre.style.backgroundColor = color;
-        pre.className = 'diffview';
-        pre.appendChild(document.createTextNode(decodeHtmlspecialChars(part.value)));
-        fragment.appendChild(pre);
-      });
+			diff.forEach((part) => {
+				if (part.added) {
+					color = '#a6f3a6';
+				} else if (part.removed) {
+					color = '#f8cbcb';
+				} else {
+					color = '';
+				}
+				pre = document.createElement('pre');
+				pre.style.backgroundColor = color;
+				pre.className = 'diffview';
+				pre.appendChild(document.createTextNode(decodeHtmlspecialChars(part.value)));
+				fragment.appendChild(pre);
+			});
 
-      display.appendChild(fragment);
-    };
+			display.appendChild(fragment);
+		};
 
 
-    const override = document.getElementById('override-pane');
-    const corePane = document.getElementById('core-pane');
-    const diffMain = document.getElementById('diff-main');
+		const override = document.getElementById('override-pane');
+		const corePane = document.getElementById('core-pane');
+		const diffMain = document.getElementById('diff-main');
 
-    const conditionalSection = document.getElementById('conditional-section');
+		const conditionalSection = document.getElementById('conditional-section');
 
-    const JformShowCore = document.getElementById('jform_show_core');
-    const JformShowDiff = document.getElementById('jform_show_diff');
+		const JformShowCore = document.getElementById('jform_show_core');
+		const JformShowDiff = document.getElementById('jform_show_diff');
 
-    if (JformShowCore && corePane) {
-      JformShowCore.addEventListener('change', (e) => {
-        const displayCore = corePane.style.display;
+		if (JformShowCore && corePane) {
+			JformShowCore.addEventListener('change', (e) => {
+				const displayCore = corePane.style.display;
 
-        if (displayCore === 'none' && e.target.id === 'jform_show_core1') {
-          corePane.style.display = 'block';
-          override.className = 'col-md-6';
-          Joomla.editors.instances.jform_core.refresh();
-        } else if (displayCore === 'block' && e.target.id === 'jform_show_core0') {
-          corePane.style.display = 'none';
-          override.className = 'col-md-12';
-        }
+				if (displayCore === 'none' && e.target.id === 'jform_show_core1') {
+					corePane.style.display = 'block';
+					override.className = 'col-md-6';
+					Joomla.editors.instances.jform_core.refresh();
+				} else if (displayCore === 'block' && e.target.id === 'jform_show_core0') {
+					corePane.style.display = 'none';
+					override.className = 'col-md-12';
+				}
 
-        const coreState = {
-          activeSwitch: e.target.id,
-        };
+				const coreState = {
+					activeSwitch: e.target.id,
+				};
 
-        if (typeof Storage !== 'undefined') {
-          localStorage.setItem('coreSwitchState', JSON.stringify(coreState));
-        }
-      });
-    }
+				if (typeof Storage !== 'undefined') {
+					localStorage.setItem('coreSwitchState', JSON.stringify(coreState));
+				}
+			});
+		}
 
-    if (JformShowDiff && diffMain) {
-      JformShowDiff.addEventListener('change', (e) => {
-        const displayDiff = diffMain.style.display;
+		if (JformShowDiff && diffMain) {
+			JformShowDiff.addEventListener('change', (e) => {
+				const displayDiff = diffMain.style.display;
 
-        if (displayDiff === 'none' && e.target.id === 'jform_show_diff1') {
-          diffMain.style.display = 'block';
-        } else if (displayDiff === 'block' && e.target.id === 'jform_show_diff0') {
-          diffMain.style.display = 'none';
-        }
+				if (displayDiff === 'none' && e.target.id === 'jform_show_diff1') {
+					diffMain.style.display = 'block';
+				} else if (displayDiff === 'block' && e.target.id === 'jform_show_diff0') {
+					diffMain.style.display = 'none';
+				}
 
-        const diffState = {
-          activeSwitch: e.target.id,
-        };
+				const diffState = {
+					activeSwitch: e.target.id,
+				};
 
-        if (typeof Storage !== 'undefined') {
-          localStorage.setItem('diffSwitchState', JSON.stringify(diffState));
-        }
-      });
-    }
+				if (typeof Storage !== 'undefined') {
+					localStorage.setItem('diffSwitchState', JSON.stringify(diffState));
+				}
+			});
+		}
 
-    const setPrestate = function setPrestate() {
-      if (typeof Storage !== 'undefined') {
-        // Fetch the Storage elements
-        const cState = JSON.parse(localStorage.getItem('coreSwitchState'));
-        const dState = JSON.parse(localStorage.getItem('diffSwitchState'));
-        let cStateActiveSwitchCore = 'jform_show_core0';
-        let cStateActiveSwitchDiff = 'jform_show_diff0';
+		const setPrestate = function setPrestate() {
+			if (typeof Storage !== 'undefined') {
+				// Fetch the Storage elements
+				const cState = JSON.parse(localStorage.getItem('coreSwitchState'));
+				const dState = JSON.parse(localStorage.getItem('diffSwitchState'));
+				let cStateActiveSwitchCore = 'jform_show_core0';
+				let cStateActiveSwitchDiff = 'jform_show_diff0';
 
-        if (cState.activeSwitch !== undefined) {
-          cStateActiveSwitchCore = cState.activeSwitch;
-        }
+				if (cState.activeSwitch !== undefined) {
+					cStateActiveSwitchCore = cState.activeSwitch;
+				}
 
-        if (cStateActiveSwitchCore === 'jform_show_core0' && JformShowCore) {
-          corePane.style.display = 'none';
-          override.className = 'col-md-12';
-        } else if (cStateActiveSwitchCore === 'jform_show_core1' && JformShowCore) {
-          corePane.style.display = 'block';
-          override.className = 'col-md-6';
-        }
+				if (cStateActiveSwitchCore === 'jform_show_core0' && JformShowCore) {
+					corePane.style.display = 'none';
+					override.className = 'col-md-12';
+				} else if (cStateActiveSwitchCore === 'jform_show_core1' && JformShowCore) {
+					corePane.style.display = 'block';
+					override.className = 'col-md-6';
+					setTimeout(function () {
+						JformShowCore.inputs[1].parentNode.classList.add("active");
+						JformShowCore.inputs[1].checked = true;
+						JformShowCore.inputs[0].checked = false;
+						JformShowCore.spans[0].classList.remove('active');
+						JformShowCore.spans[1].classList.add('active');
+					}, 500);
+				}
 
-        if (dState.activeSwitch !== undefined) {
-          cStateActiveSwitchDiff = dState.activeSwitch;
-        }
+				if (dState.activeSwitch !== undefined) {
+					cStateActiveSwitchDiff = dState.activeSwitch;
+				}
 
-        if (cStateActiveSwitchDiff === 'jform_show_diff0' && JformShowDiff) {
-          diffMain.style.display = 'none';
-		  setTimeout(function () {
-					console.log(JformShowDiff.spans);
-			}, 500);
-          // console.log('Todo set state of jform_show_diff');
-        } else if (cStateActiveSwitchDiff === 'jform_show_diff1' && JformShowDiff) {
-          diffMain.style.display = 'block';
-		  setTimeout(function () {
-					console.log(JformShowDiff.spans);
-			}, 500);
+				if (cStateActiveSwitchDiff === 'jform_show_diff0' && JformShowDiff) {
+					diffMain.style.display = 'none';
+				} else if (cStateActiveSwitchDiff === 'jform_show_diff1' && JformShowDiff) {
+					diffMain.style.display = 'block';
+					setTimeout(function () {
+						JformShowDiff.inputs[1].parentNode.classList.add("active");
+						JformShowDiff.inputs[1].checked = true;
+						JformShowDiff.inputs[0].checked = false;
+						JformShowDiff.spans[0].classList.remove('active');
+						JformShowDiff.spans[1].classList.add('active');
+					}, 500);
+				}
+			}
+		};
 
-          // console.log('Todo set state of jform_show_diff');
-        }
-      }
-    };
+		if (JformShowCore || JformShowDiff) {
+			setPrestate();
+		} else if (override && conditionalSection) {
+			conditionalSection.className = 'col-md-12';
+			override.className = 'col-md-12';
+		}
 
-    if (JformShowCore || JformShowDiff) {
-      setPrestate();
-    } else if (override && conditionalSection) {
-      conditionalSection.className = 'col-md-12';
-      override.className = 'col-md-12';
-    }
+		const diffs = [].slice.call(document.querySelectorAll('#original'));
 
-    const diffs = [].slice.call(document.querySelectorAll('#original'));
-
-    for (let i = 0, l = diffs.length; i < l; i += 1) {
-      compare(diffs[i], diffs[i].nextElementSibling);
-    }
-  });
+		for (let i = 0, l = diffs.length; i < l; i += 1) {
+			compare(diffs[i], diffs[i].nextElementSibling);
+		}
+	});
 }());
